03dbb4baa3ef67f47d3ea1b7b1f4eb668c1c1af4
vitorcastilho <vitorcastilho@msn.com>
2020-11-24 13:16:27 -0300

    card criado

---
+++ src/components/Promotion/Card/Card.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Card.css'
+
+const PromotionCard = ({ promotion }) => {
+    return (
+        <div className="promotion-card">
+            <img className="promotion-card__img" src={promotion.imageUrl} />
+            <div className="promotion-card__info">
+                <h1 className="promotion-card__title">
+                    {promotion.title}
+                </h1>
+                <span className='promotion-card__price'>
+                    {promotion.price}
+                </span>
+                <footer className='promotion-card__footer'>
+                    {promotion.comments.length > 0 && (
+                        <div className="promotion-card__comment">"{promotion.comments[0].comment}"</div>
+                    )}
+
+                    <div className='promotion-card__comments-count'>
+                        {promotion.comments.length} Comentário{promotion.comments.length > 1 ? 's' : null}
+                    </div>
+                    <a className='promotion-card__link' href={promotion.url} TARGET="_blanck" >
+                        IR PARA O SITE
+                    </a>
+                </footer>
+            </div>
+        </div>
+    )
+}
+
+export default PromotionCard;
